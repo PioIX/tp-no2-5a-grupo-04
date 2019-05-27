224bc78e790070f44cd86f0c56adf52b4a19bc8e
rociofb <49161709+rociofb@users.noreply.github.com>
2019-05-27 12:52:42 -0300

    primer commit
    primer commit

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Bingo
+    Dim Y As Integer
+    Dim x As Integer
+    Dim z As Integer
+    Dim Temp As Integer
+    Private Sub Bingo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        Next
+
+    End Sub
+
+    Private Sub Button37_Click(sender As Object, e As EventArgs) Handles Button37.Click
+
+    End Sub
+End Class
